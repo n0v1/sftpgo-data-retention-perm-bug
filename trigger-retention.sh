3da04e6ab9435395ad35066c956be55e67c3df90
Christopher Fenn <cfenn@vwd.com>
2024-03-13 12:06:59 +0100

    reproduce issue

--- trigger-retention.sh
+++ trigger-retention.sh
@@ -5,4 +5,4 @@ set -e
 TOKEN=$(curl --fail --silent --show-error --user admin:nimda --request GET http://localhost:8080/api/v2/token | jq --raw-output .access_token)
 
 # trigger data retention check
-curl --fail --silent --show-error --header "Authorization: Bearer ${TOKEN}" --request POST http://localhost:8080/api/v2/eventrules/run/delete-old-data-user1
+curl --fail --silent --show-error --header "Authorization: Bearer ${TOKEN}" --request POST http://localhost:8080/api/v2/eventrules/run/delete-old-files-user1
