f6f173f826d453703194035022231761c5f36054
Christopher Fenn <cfenn@vwd.com>
2024-03-13 11:33:57 +0100

    add script for triggering data retention check

---
+++ trigger-retention.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -e
+
+# Get auth token
+TOKEN=$(curl --fail --silent --show-error --user admin:nimda --request GET http://localhost:8080/api/v2/token | jq --raw-output .access_token)
+
+# trigger data retention check
+curl --fail --silent --show-error --header "Authorization: Bearer ${TOKEN}" --request POST http://localhost:8080/api/v2/eventrules/run/delete-old-data-user1
